091cb1a8ee020019d3f8b1d3ebe2fd62d4d64da4
andreyvpng <andrewvarfolomeev@protonmail.com>
2018-10-27 13:03:36 +1000

    update

--- main.js
+++ main.js
@@ -1,19 +1,53 @@
 'user strict';
 
+var ls = window.top.localStorage;
+
 var input_text = document.querySelector('#input-text'),
     todo_list = document.querySelector('.todo-list'),
-    delete_completed_tasks = document.querySelector('#delete-completed-tasks');
+    delete_completed_tasks = document.querySelector('#delete-completed-tasks'),
+    top_id = parseInt(ls.getItem("top_id")),
+    ctx = JSON.parse(ls.getItem("ctx"));
 
+if (!top_id) top_id = 1;
+if (!ctx) ctx = [];
+
+
+function updateLocalStorage() { 
+  top_id++;
+  ls.setItem("top_id", ''+top_id);
+  console.log(JSON.stringify(ctx));
+  ls.setItem("ctx", JSON.stringify(ctx));
+}
+
+
+function deleteFromCtx(item) {
+  console.log(item.id); 
+  for (var i = 0; i < ctx.length; i++) {
+    if (item.id == ctx[i].id) {
+      ctx.splice(i, 1);
+    }
+  }
+  updateLocalStorage();
+  item.remove();
+}
 
 // add new element, when enter is pressed
 input_text.addEventListener('keypress', function(event) {
   if (event.keyCode == 13) {
     if (input_text.value !== '') {
       appendNewItem({
-        id: NaN,
+        id: top_id,
         text: input_text.value
       });
     }
+
+    ctx.push({
+      'id': top_id,
+      'text': input_text.value
+    });
+    
+    updateLocalStorage();
+
     input_text.value = "";
   }
 });
@@ -24,7 +58,8 @@ delete_completed_tasks.addEventListener('click', function() {
     var item = todo_list.children[i];
 
     if (~item.classList.value.split(' ').indexOf('complete')) {
-      item.remove();
+      console.log(item.id);
+      deleteFromCtx(item);
       i--;
     }
   }
@@ -47,8 +82,9 @@ function getTodoItem(obj) {
   });
 
   close_button.addEventListener('click', function(event) {
-    var id = wrapper.id;
-    wrapper.remove();
+    //var id = wrapper.id;
+    deleteFromCtx(wrapper);
+    //wrapper.remove();
   });
 
   wrapper.appendChild(text);
@@ -60,4 +96,8 @@ function getTodoItem(obj) {
 
 function appendNewItem(obj) {
   todo_list.insertBefore(getTodoItem(obj), todo_list.firstChild);
+}
+
+for (var i = 0; i < ctx.length; i++) {
+  appendNewItem(ctx[i]);
 }
